db150c5718d72dceb493994544de1e647a2e227d
mandykhaira <>
2026-03-19 23:47:12 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,8 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1= "milk"
+dairy2= "cheese"
+bakery1= "bread"
+message=f"{grocery_items[0:4]},{grocery_items[5:11]},{grocery_items[12:17]}"
+print(message)
+aisle=5
+print(f"We have dairy and bakery items: {dairy1},{dairy2}, and {bakery1} in aisle {aisle}")
